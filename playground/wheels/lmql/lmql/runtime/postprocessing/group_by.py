f61c24e5971134c01463e47a0b55a3566cb64f45
lbeurerkellner <lucabeurerkellner@gmail.com>
2023-03-10 16:37:58 +0000

    Deploying to gh-pages from @ lbeurerkellner/green-gold-dachshund-web@95e36b6ebc0fd52bece9796bb4e756ea68969a2a 🚀

--- playground/wheels/lmql/lmql/runtime/postprocessing/group_by.py
+++
@@ -1,8 +0,0 @@
-from lmql.runtime.decoder_head import DecoderHead
-
-class GroupByPostprocessor:
-    def __init__(self):
-        pass
-
-    async def process(self, results, interpreter):
-        raise NotImplementedError()
