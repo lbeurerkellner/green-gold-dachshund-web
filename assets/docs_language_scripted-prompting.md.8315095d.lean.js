f6540f973a899bb64a889ff9b476dd94b1700ae0
lbeurerkellner <lucabeurerkellner@gmail.com>
2023-10-09 18:21:12 +0000

    Deploying to web from @ lbeurerkellner/green-gold-dachshund-web@5c8963d3b197595f1b07d5b2527cea9052a2a752 🚀

---
+++ assets/docs_language_scripted-prompting.md.8315095d.lean.js
@@ -0,0 +1,49 @@
+import{_ as n,o as e,c as o,Q as t,k as s,a}from"./chunks/framework.c2adf1ba.js";const j=JSON.parse('{"title":"Scripted Prompting","description":"","frontmatter":{"order":1},"headers":[],"relativePath":"docs/language/scripted-prompting.md","filePath":"docs/language/scripted-prompting.md"}'),p={name:"docs/language/scripted-prompting.md"},l=t("",7),r=s("div",{class:"language-promptdown vp-adaptive-theme"},[s("button",{title:"Copy Code",class:"copy"}),s("span",{class:"lang"},"promptdown"),s("pre",{"pd-text":`A list of things not to forget when going to the sea (not travelling):
+[LIST|-A phone with call, texting or tech services
+-A copy of the local paper
+-A pen or phone Keytar
+]
+`,animate:"true",__animate:"true","animate-speed":"50",class:"promptdown promptdown-compiled",style:{opacity:"1"}},[s("p",{"pd-shadow-id":"556",text:"A","pd-insertion-point":"true"},[a(`A list of things not to forget when going to the sea (not travelling):
+`),s("span",{"pd-shadow-id":"558","pd-instant":"false",text:"",class:"promptdown-var color-red"},[s("span",{"pd-shadow-id":"559",text:"L",class:"promptdown-var-name"},"LIST"),a(`-A phone with call, texting or tech services
+-A copy of the local paper
+-A pen or phone Keytar
+`)]),a(`
+`)])])],-1),i=t("",4),d=s("div",{class:"language-promptdown vp-adaptive-theme"},[s("button",{title:"Copy Code",class:"copy"}),s("span",{class:"lang"},"promptdown"),s("pre",{"pd-text":`A list of things not to forget when going to the sea (not travelling):
+-[THING|A phone with a/r text]
+-[THING|pletter]
+-[THING|accoon]
+-[THING|Films about/of the sea]
+-[THING|A has been in the Poconos for/ Entered the Poconos]
+`,animate:"true",__animate:"true","animate-speed":"50",class:"promptdown promptdown-compiled",style:{opacity:"1"}},[s("p",{"pd-shadow-id":"565",text:"A","pd-insertion-point":"true"},[a(`A list of things not to forget when going to the sea (not travelling):
+-`),s("span",{"pd-shadow-id":"567","pd-instant":"false",text:"",class:"promptdown-var color-lightorange"},[s("span",{"pd-shadow-id":"568",text:"T",class:"promptdown-var-name"},"THING"),a("A phone with a/r text")]),a(`
+-`),s("span",{"pd-shadow-id":"573","pd-instant":"false",text:"",class:"promptdown-var color-lightorange"},[s("span",{"pd-shadow-id":"574",text:"T",class:"promptdown-var-name"},"THING"),a("pletter")]),a(`
+-`),s("span",{"pd-shadow-id":"579","pd-instant":"false",text:"",class:"promptdown-var color-lightorange"},[s("span",{"pd-shadow-id":"580",text:"T",class:"promptdown-var-name"},"THING"),a("accoon")]),a(`
+-`),s("span",{"pd-shadow-id":"585","pd-instant":"false",text:"",class:"promptdown-var color-lightorange"},[s("span",{"pd-shadow-id":"586",text:"T",class:"promptdown-var-name"},"THING"),a("Films about/of the sea")]),a(`
+-`),s("span",{"pd-shadow-id":"591","pd-instant":"false",text:"",class:"promptdown-var color-lightorange"},[s("span",{"pd-shadow-id":"592",text:"T",class:"promptdown-var-name"},"THING"),a("A has been in the Poconos for/ Entered the Poconos")]),a(`
+`)])])],-1),c=t("",4),h=s("div",{class:"language-promptdown vp-adaptive-theme"},[s("button",{title:"Copy Code",class:"copy"}),s("span",{class:"lang"},"promptdown"),s("pre",{"pd-text":`A list of things not to forget when going to the sea (not travelling):
+-[THING|A good pair of blue/gel saskaers]
+-[THING|A good sun tanner]
+-[THING|A good air freshener]
+-[THING|A good spot for washing your hands]
+-[THING|A good spot for washing your feet]
+`,animate:"true",__animate:"true","animate-speed":"50",class:"promptdown promptdown-compiled",style:{opacity:"1"}},[s("p",{"pd-shadow-id":"598",text:"A","pd-insertion-point":"true"},[a(`A list of things not to forget when going to the sea (not travelling):
+-`),s("span",{"pd-shadow-id":"600","pd-instant":"false",text:"",class:"promptdown-var color-lightorange"},[s("span",{"pd-shadow-id":"601",text:"T",class:"promptdown-var-name"},"THING"),a("A good pair of blue/gel saskaers")]),a(`
+-`),s("span",{"pd-shadow-id":"606","pd-instant":"false",text:"",class:"promptdown-var color-lightorange"},[s("span",{"pd-shadow-id":"607",text:"T",class:"promptdown-var-name"},"THING"),a("A good sun tanner")]),a(`
+-`),s("span",{"pd-shadow-id":"612","pd-instant":"false",text:"",class:"promptdown-var color-lightorange"},[s("span",{"pd-shadow-id":"613",text:"T",class:"promptdown-var-name"},"THING"),a("A good air freshener")]),a(`
+-`),s("span",{"pd-shadow-id":"618","pd-instant":"false",text:"",class:"promptdown-var color-lightorange"},[s("span",{"pd-shadow-id":"619",text:"T",class:"promptdown-var-name"},"THING"),a("A good spot for washing your hands")]),a(`
+-`),s("span",{"pd-shadow-id":"624","pd-instant":"false",text:"",class:"promptdown-var color-lightorange"},[s("span",{"pd-shadow-id":"625",text:"T",class:"promptdown-var-name"},"THING"),a("A good spot for washing your feet")]),a(`
+`)])])],-1),g=t("",4),u=s("div",{class:"language-promptdown vp-adaptive-theme"},[s("button",{title:"Copy Code",class:"copy"}),s("span",{class:"lang"},"promptdown"),s("pre",{"pd-text":`A list of things not to forget when going to the sea (not travelling): ⏎
+-[THING|Sunscreen]⏎
+-[THING|Beach Towels]⏎
+-[THING|Beach Umbrella]⏎
+-[THING|Beach Chairs]⏎
+-[THING|Beach Bag]⏎
+The most essential of which is: [ESSENTIAL_THING(color)|Sunscreen]
+`,animate:"true",__animate:"true","animate-speed":"50",class:"promptdown promptdown-compiled",style:{opacity:"1"}},[s("p",{"pd-shadow-id":"631",text:"A","pd-insertion-point":"true"},[a(`A list of things not to forget when going to the sea (not travelling): ⏎
+-`),s("span",{"pd-shadow-id":"633","pd-instant":"false",text:"",class:"promptdown-var color-lightorange"},[s("span",{"pd-shadow-id":"634",text:"T",class:"promptdown-var-name"},"THING"),a("Sunscreen")]),a(`⏎
+-`),s("span",{"pd-shadow-id":"639","pd-instant":"false",text:"",class:"promptdown-var color-lightorange"},[s("span",{"pd-shadow-id":"640",text:"T",class:"promptdown-var-name"},"THING"),a("Beach Towels")]),a(`⏎
+-`),s("span",{"pd-shadow-id":"645","pd-instant":"false",text:"",class:"promptdown-var color-lightorange"},[s("span",{"pd-shadow-id":"646",text:"T",class:"promptdown-var-name"},"THING"),a("Beach Umbrella")]),a(`⏎
+-`),s("span",{"pd-shadow-id":"651","pd-instant":"false",text:"",class:"promptdown-var color-lightorange"},[s("span",{"pd-shadow-id":"652",text:"T",class:"promptdown-var-name"},"THING"),a("Beach Chairs")]),a(`⏎
+-`),s("span",{"pd-shadow-id":"657","pd-instant":"false",text:"",class:"promptdown-var color-lightorange"},[s("span",{"pd-shadow-id":"658",text:"T",class:"promptdown-var-name"},"THING"),a("Beach Bag")]),a(`⏎
+The most essential of which is: `),s("span",{"pd-shadow-id":"663","pd-instant":"false",text:"",class:"promptdown-var color-ochre"},[s("span",{"pd-shadow-id":"664",text:"E",class:"promptdown-var-name"},"ESSENTIAL_THING"),a("Sunscreen")]),a(`
+`)])])],-1),m=t("",7),w=[l,r,i,d,c,h,g,u,m];function T(f,_,v,y,I,N){return e(),o("div",null,w)}const q=n(p,[["render",T]]);export{j as __pageData,q as default};
